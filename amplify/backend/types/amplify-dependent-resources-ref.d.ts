bb117bee30cbe9154a4dfa1866dc6c892be3a4e4
rjabhi <rjabhi@amazon.com>
2025-04-28 21:46:00 -0700

    initial gen1 commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "testrefactorstorage7cfedadc": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s346d3006b": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
